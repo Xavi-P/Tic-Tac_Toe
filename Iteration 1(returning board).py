ead740bae4d349bea9110a885abcf020723b61e0
Xavi-P <Xman08711@gmail.com>
2026-01-09 18:23:42 -0500

    Create Iteration 1(returning board).py

---
+++ Iteration 1(returning board).py
@@ -0,0 +1,12 @@
+def create_board():
+  return [["1","2","3"],["4" ,"5","6"],["7","8","9"]]
+# def display_board(board): 
+#   for row in board:
+#     print(row)
+# def get_user_move(board):
+#   user_row=int(input('which row would you like to play: 1, 2, or 3?'))
+#   board.insert()
+  
+board=create_board()
+# display_board(board)
+# get_user_move(board)
